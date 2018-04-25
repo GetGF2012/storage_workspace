66f4f9149259260edb9a152ad641a795996dcdba
trentino123 <34720798+trentino123@users.noreply.github.com>
2018-04-25 14:00:50 +0000

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
 # manifests/init.pp
 class storage_workspace {
-  user { 'luchianoforever':
+  user { 'elmo_has_left_the_building':
     ensure => 'present',
   }
 }
+# luchiano change april 25th 2018, room HL West 6th 294
